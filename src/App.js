3f333e8edb38c1097cf98aa61140795fca029d14
PolinaYartseva <polina_yartseva@hotmail.com>
2020-08-05 18:54:44 +0200

    1st commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { count, length } from './numbers';
+
+const number = 35;
+const logic = true;
+const ternar = true;
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p style={{
+          color: 'yellow',
+          fontSize: 10
+        }}>
+          Hello, World!
+        </p>
+        <p>
+          {number}
+        </p>
+        <p>
+          {100 + 50}
+        </p>
+        <p>
+          {logic && "It's true"}
+        </p>
+        <p>
+          {ternar ? 'ternar is true' : 'ternat is false'}
+        </p>
+        <p>
+          {undefined}
+          {null}
+          {true}
+          {false}
+        </p>
+        <p>
+          {count * length}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
