05961d9927e0a8a1a89cb29927bbda4a8aff2460
PolinaYartseva <polina_yartseva@hotmail.com>
2020-08-09 01:12:56 +0200

    21st homework

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,25 @@ import ItemList from "../ItemList/ItemList";
 import InputItem from "../InputItem/InputItem";
 import Footer from "../Footer/Footer";
 
-const todoItem = 'Выполнить дз №19';
-const App = () => (
+const App = () => {
+    const items = [
+        {
+            value: 'Выполнить дз №19'
+        },
+        {
+            value: 'Выполнить дз №20'
+        },
+        {
+            value: 'Прослушать вебинар'
+        }
+    ];
+    return (
     <div>
         <h1>Важные дела:</h1>
         <InputItem />
-        <ItemList todoItem={todoItem}/>
-        <Footer count={5} />
+        <ItemList items={items}/>
+        <Footer count={1} />
     </div>);
+}
 
 export default App;
