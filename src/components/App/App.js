46e868f051be3a42f6ddf803821c5fb56a72f1ff
PolinaYartseva <polina_yartseva@hotmail.com>
2020-08-10 00:45:38 +0200

    refactoring 27th homework

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,28 +5,30 @@ import Footer from "../Footer/Footer";
 import styles from './App.module.css';
 import './fonts/fonts.css';
 
-const App = () => {
-    const items = [
-        {
-            value: 'Выполнить дз №19',
-            isDone: true
-        },
-        {
-            value: 'Выполнить дз №20',
-            isDone: true
-        },
-        {
-            value: 'Прослушать вебинар',
-            isDone: false
-        }
-    ];
-    return (
-    <div className={styles.wrap}>
-        <h1 className={styles.title}>список дел</h1>
-        <InputItem />
-        <ItemList items={items} />
-        <Footer count={1} />
-    </div>);
+class App extends React.Component {
+    render() {
+        const items = [
+            {
+                value: 'Выполнить дз №19',
+                isDone: true
+            },
+            {
+                value: 'Выполнить дз №20',
+                isDone: true
+            },
+            {
+                value: 'Прослушать вебинар',
+                isDone: false
+            }
+        ];
+        return (
+            <div className={styles.wrap}>
+                <h1 className={styles.title}>список дел</h1>
+                <InputItem />
+                <ItemList items={items} />
+                <Footer count={1} />
+            </div>);
+    }
 }
 
 export default App;
