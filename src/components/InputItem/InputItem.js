c08207065300c14dbf3fd1865d8a8b9f2af8db8e
PolinaYartseva <polina_yartseva@hotmail.com>
2020-08-09 11:25:48 +0200

    25th homework

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,5 +1,12 @@
 import React from "react";
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (<input/>);
+const InputItem = () => (<div>
+    <TextField
+        label="Введите название дела"
+        id="margin-normal"
+        margin="normal"
+    />
+</div>);
 
 export default InputItem;
