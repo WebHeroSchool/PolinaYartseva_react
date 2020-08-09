bd7383aaa5d73ca8e5b541f08f4559dfade1b990
PolinaYartseva <polina_yartseva@hotmail.com>
2020-08-10 00:36:35 +0200

    styling 26th homework

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,10 +1,22 @@
 import React from "react";
 import Item from "../Item/Item";
+import Checkbox from '@material-ui/core/Checkbox';
+import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
+import styles from "../ItemList/ItemList.module.css";
 
-const ItemList = ({ items }) => (<ul>
-    {items.map(item => <li key={item.value}>
-        <Item value={item.value} isDone={item.isDone} />
-    </li>)}
-</ul>);
+const ItemList = ({ items }) => (
+    <ul className={styles.itemList}> {
+        items.map(item =>
+            <li key={item.value}>
+                <Checkbox
+                    inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} />
+                <Item
+                    value={item.value}
+                    isDone={item.isDone} />
+                <div className={styles.delete}>
+                    <DeleteOutlinedIcon />
+                </div>
+            </li>)}
+    </ul>);
 
 export default ItemList;
